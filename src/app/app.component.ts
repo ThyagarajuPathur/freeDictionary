d35a738ed7d14b59d4d4c671146b9fbdaf54f1ef
Thyagaraju <thyagaraju.ptr@gmail.com>
2022-03-26 03:06:26 +0530

    stable 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, NgModule,OnChanges, SimpleChange } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import axios from 'axios';
+import * as $ from 'jquery';
+import { debounceTime } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'freeDictionary';
+  word = '';
+  result = '';
+  data: any;
+  isDark: boolean = true;
+  switchColor: string = 'black';
+  audioUrl: string = '';
+  searchMeaning: FormControl = new FormControl();
+  selectedLanguage:string='English';
+  //showPlayer: boolean = false;
+  defNo:number=0;
+
+  ngOnInit() {
+    // if (this.audioUrl != '') {
+    //   this.showPlayer = true;
+    // }
+
+    this.searchMeaning.valueChanges.pipe(debounceTime(700)).subscribe({
+      next: (value) => this.findMeaning(value),
+    });
+  }
+
+  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+    // if (
+    //   changes['audioUrl'].previousValue !== changes['audioUrl'].currentValue &&
+    //   changes['audioUrl'].currentValue !== ''
+    // ) {
+    //   this.showPlayer = false;
+    //   setTimeout(() => (this.showPlayer = true), 0);
+    // }
+  }
+
+  findMeaning(value: string) {
+    if (value === '') {
+      this.data = [];
+    }
+    let word = value;
+    axios
+      .get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+      .then((res) => {
+        this.data = res.data;
+        let audios = res.data.find((x) =>
+          x.phonetics.find((y) => y.audio.length > 0)
+        );
+        this.audioUrl = audios.phonetics.find(x=>x.audio.length>0).audio;
+        console.log(audios);
+        console.log(this.audioUrl);
+        console.log(this.data);
+      }).catch(error=>{
+        this.audioUrl='';
+        this.data = [];
+        console.log(error)
+      })
+
+  }
+
+  toggleDark(event: any) {
+    if (event) {
+      $('.app').css('background-color', 'rgb(40, 44, 52)');
+      $('.app').css('color', 'white');
+      this.switchColor = 'black';
+    } else {
+      $('.app').css('background-color', 'rgb(255, 255, 255)');
+      $('.app').css('color', 'black');
+      this.switchColor = 'white';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiSwitchModule } from 'ngx-ui-switch';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    UiSwitchModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
