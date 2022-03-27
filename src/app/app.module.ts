5c780e0330bbf5ff193162db97fe9861a30c0878
Thyagaraju <thyagaraju.ptr@gmail.com>
2022-03-28 01:20:57 +0530

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,28 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChe-F4BHJeZtjMtRb-S1xL4Ejv_QCoQeE",
+  authDomain: "freedictionary-5889.firebaseapp.com",
+  projectId: "freedictionary-5889",
+  storageBucket: "freedictionary-5889.appspot.com",
+  messagingSenderId: "680539932336",
+  appId: "1:680539932336:web:b247eab25fe8d312349d75",
+  measurementId: "G-6HZN2QPRQ6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 @NgModule({
   declarations: [AppComponent],
   imports: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+  apiKey: "AIzaSyChe-F4BHJeZtjMtRb-S1xL4Ejv_QCoQeE",
+  authDomain: "freedictionary-5889.firebaseapp.com",
+  projectId: "freedictionary-5889",
+  storageBucket: "freedictionary-5889.appspot.com",
+  messagingSenderId: "680539932336",
+  appId: "1:680539932336:web:b247eab25fe8d312349d75",
+  measurementId: "G-6HZN2QPRQ6"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
